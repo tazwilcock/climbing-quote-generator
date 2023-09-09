f7fd45d75241ba21bc800c59370c55ee609b4b80
tazwilcock <twdevdesign@gmail.com>
2023-09-08 21:49:46 -0600

    add btns functionality

--- script.js
+++ script.js
@@ -4,28 +4,29 @@ const authorText = document.getElementById('author')
 const xBtn = document.getElementById('x')
 const newQuoteBtn = document.getElementById('new-quote')
 
-// let apiQuotes = [];
-
 // Show New Quote
 function newQuote() {
-      // Pick a random quote from apiQuotes array
+      // Pick a random quote from localQuotes file
       const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
       authorText.textContent = quote.author
+
+      // Check quote length and determine the styling
+      if (quote.text.length > 110) {
+            quoteText.classList.add('long-quote')
+      } else {
+            quoteText.classList.remove('long-quote')
+      }
       quoteText.textContent = quote.text
 }
 
-// Get Quotes From API
-// async function getQuotes() {
-//       const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
-//       try {
-//             const response = await fetch(apiUrl)
-//             apiQuotes = await response.json();
-//             newQuote();
-//       } catch (error) {
-//             // handle your error
-//       }
-// }
+// Tweet a Quote
+function tweetQuote() {
+      const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+      window.open(twitterUrl, '_blank')
+}
 
-// On Load
-// getQuotes();
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+xBtn.addEventListener('click', tweetQuote)
+
 newQuote();
