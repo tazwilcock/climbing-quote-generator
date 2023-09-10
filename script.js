7a6f82318d3562aaca0ded42d4fb73233d0df82b
tazwilcock <twdevdesign@gmail.com>
2023-09-09 22:24:57 -0600

    add spinner

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const quoteBox = document.getElementById('quote-box');
+const quoteBox = document.getElementById('quote-box')
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author')
 const xBtn = document.getElementById('x')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
+// Show Loading
+function loading() {
+      loader.hidden = false;
+      quoteBox.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+      quoteBox.hidden = false;
+      loader.hidden = true;
+}
 // Show New Quote
 function newQuote() {
+      loading();
       // Pick a random quote from localQuotes file
       const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
       authorText.textContent = quote.author
@@ -17,6 +30,7 @@ function newQuote() {
             quoteText.classList.remove('long-quote')
       }
       quoteText.textContent = quote.text
+      complete();
 }
 
 // Tweet a Quote
